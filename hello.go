22c957669a20fadd4543e366f4254b25ce5879c3
izumiiii <41711526+izumiiii@users.noreply.github.com>
2022-05-30 00:29:35 +0900

    a

--- hello.go
+++ hello.go
@@ -28,7 +28,7 @@ const (
 	tmplBody = `
 {{define "` + tmplName + `" -}}
 <html><body>
-<p>update!7</p>
+<p>update!8</p>
 Version {{.V}} : {{if .C.Risky}}<em>{{end}}
 {{- .C.Greeting}}{{if .C.Risky}}</em>{{end}} {{.P}}
 </body></html>
